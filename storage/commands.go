bf3d12b2311bd6a5792a41957691ac73aec4ab6a
Ottony Costa <ottony93@gmail.com>
2016-11-10 04:22:52 -0300

    Add some basic Redis' commands
    Signed-off-by: Bruno Henrique - Garu <squall.bruno@gmail.com>

--- storage/commands.go
+++ storage/commands.go
@@ -1,6 +1,7 @@
 package storage
 
 import (
+	"errors"
 	"strconv"
 )
 
@@ -143,4 +144,83 @@ func (s *Storage) MSetNX(kvs ...string) bool {
 
 func (s *Storage) StrLen(k string) int {
 	return len(s.state[k])
+}
+
+func (s *Storage) Del(ks ...string) int {
+	t := 0
+
+	for i := range ks {
+		if _, ok := s.state[ks[i]]; ok {
+			t++
+			delete(s.state, ks[i])
+		}
+	}
+
+	return t
+}
+
+func (s *Storage) FlushAll() {
+	s.state = make(map[string]string)
+}
+
+func (s *Storage) Exists(k string) bool {
+	_, ok := s.state[k]
+
+	return ok
+}
+
+func (s *Storage) GetRange(k string, start, end int) string {
+	length := len(s.state[k])
+	v, ok := s.state[k]
+	if !ok {
+		return ""
+	}
+
+	if start < 0 {
+		start = length + start
+		if start < 0 {
+			start = 0
+		}
+	}
+
+	if end < 0 {
+		end = length + end
+	}
+
+	if end > length {
+		end = length - 1
+	}
+
+	return v[start : end+1]
+}
+
+func (s *Storage) Rename(k, nk string) (bool, error) {
+	if _, ok := s.state[k]; !ok {
+		return false, errors.New("ERR no such key")
+	}
+
+	v := s.state[k]
+	delete(s.state, k)
+	s.state[nk] = v
+
+	return true, nil
+}
+
+func (s *Storage) RenameNX(k, nk string) (bool, error) {
+	if _, ok := s.state[nk]; ok {
+		return false, nil
+	}
+
+	if ok, err := s.Rename(k, nk); !ok {
+		return ok, err
+	}
+
+	return true, nil
+}
+
+func (s *Storage) GetSet(k, v string) string {
+	oldV := s.state[k]
+	s.state[k] = v
+
+	return oldV
 }
--- storage/commands_test.go
+++ storage/commands_test.go
@@ -150,3 +150,145 @@ func TestStrLen(t *testing.T) {
 		t.Error("must return 5")
 	}
 }
+
+func TestDel(t *testing.T) {
+	s := storage.New()
+
+	s.MSetNX("key1", "value1", "key2", "value2")
+	if v := s.Del("key1", "key3"); v != 1 {
+		t.Errorf("must return the total of deleted keys")
+	}
+
+	if v := s.Get("key1"); v != "" {
+		t.Errorf("key1 must have been deleted")
+	}
+
+	if v := s.Get("key2"); v != "value2" {
+		t.Errorf("key2 must not have been deleted")
+	}
+}
+
+func TestFlushAll(t *testing.T) {
+	s := storage.New()
+
+	s.MSetNX("key1", "value1", "key2", "value2")
+	s.FlushAll()
+
+	keys := [2]string{"key1", "key2"}
+	for i, _ := range keys {
+		if v := s.Get(keys[i]); v != "" {
+			t.Errorf("%s must have been deleted", keys[i])
+		}
+	}
+}
+
+func TestExists(t *testing.T) {
+	s := storage.New()
+
+	s.Set("key", "value")
+
+	if v := s.Exists("key"); !v {
+		t.Errorf("must return true when existing key")
+	}
+
+	if v := s.Exists("nkey"); v {
+		t.Errorf("must return false when non existent key")
+	}
+}
+
+func TestGetRange(t *testing.T) {
+	s := storage.New()
+
+	s.Set("key", "This is a string")
+
+	if v := s.GetRange("key", 0, 3); v != "This" {
+		t.Errorf("must return 'This' but '%s' was returned", v)
+	}
+
+	if v := s.GetRange("key", -3, -1); v != "ing" {
+		t.Errorf("must return 'ing' but '%s' was returned", v)
+	}
+
+	if v := s.GetRange("key", 0, -1); v != "This is a string" {
+		t.Errorf("must return 'This is a string' but '%s' was returned", v)
+	}
+
+	if v := s.GetRange("key", 10, 100); v != "string" {
+		t.Errorf("must return 'string' but '%s' was returned", v)
+	}
+
+	if v := s.GetRange("key", -100, 100); v != "This is a string" {
+		t.Errorf("must return 'This is a string' but '%s' was returned", v)
+	}
+
+	if v := s.GetRange("key", 0, 0); v != "T" {
+		t.Errorf("must return 'T' but '%s' was returned", v)
+	}
+
+	if v := s.GetRange("nkey", 0, 2); v != "" {
+		t.Errorf("must return an empty string but '%s' was returned", v)
+	}
+}
+
+func TestRename(t *testing.T) {
+	s := storage.New()
+
+	s.Set("key", "value")
+
+	if ok, _ := s.Rename("key", "newKey"); !ok {
+		t.Errorf("must return true and renamed key")
+	}
+
+	if v := s.Get("newKey"); v != "value" {
+		t.Errorf("newKey must contain the old key value")
+	}
+
+	if v := s.Get("key"); v != "" {
+		t.Errorf("key must have been deleted")
+	}
+
+	if _, err := s.Rename("nkey", "key1"); err == nil {
+		t.Errorf("must return an error when non existent key")
+	}
+}
+
+func TestRenameNX(t *testing.T) {
+	s := storage.New()
+
+	s.MSet("key1", "value1", "key2", "value2", "key3", "value3")
+	if ok, _ := s.RenameNX("key1", "newKey"); !ok {
+		t.Errorf("must return true and renamed key")
+	}
+
+	if ok, _ := s.RenameNX("key2", "key3"); ok {
+		t.Errorf("must return false when have conflicting keys")
+	}
+
+	if v := s.Get("newKey"); v != "value1" {
+		t.Errorf("newKey must contain the old key value")
+	}
+
+	if v := s.Get("key1"); v != "" {
+		t.Errorf("key1 must have been deleted")
+	}
+
+	if _, err := s.RenameNX("nkey", "key1"); err == nil {
+		t.Errorf("must return an error when non existent key")
+	}
+}
+
+func TestGetSet(t *testing.T) {
+	s := storage.New()
+
+	if v := s.GetSet("key", "value1"); v != "" {
+		t.Errorf("must return an empty string")
+	}
+
+	if v := s.Get("key"); v != "value1" {
+		t.Errorf("must return 'value1'")
+	}
+
+	if v := s.GetSet("key", "value2"); v != "value1" {
+		t.Errorf("must return old value")
+	}
+}
